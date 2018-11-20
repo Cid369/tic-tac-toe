b12901e8aa495244f5bc371e179c241012dab485
Cid369 <waikit.tong.82@gmail.com>
2018-11-20 16:05:02 -0500

    * Fixed the mix up links for development and production

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://tic-tac-toe-wdi.herokuapp.com/',
+  production: 'https://aqueous-atoll-85096.herokuapp.com',
   development: 'https://tic-tac-toe-wdi.herokuapp.com/'
 }
 
